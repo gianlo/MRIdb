63c32c4f31d7e4aaa065f688e27c41cb03ab1faa
Gianlorenzo Fagiolo <gianlo@gmail.com>
2014-03-18 16:41:50 +0000

    Polished client

--- app/util/ProjectAssociationImportClient.java
+++ app/util/ProjectAssociationImportClient.java
@@ -11,32 +11,11 @@ import models.Study;
 
 
 public class ProjectAssociationImportClient {
-	private String filename;
-	
-	public String getFilename() {
-		return filename;
-	}
 
-	public void setFilename(String filename) {
-		this.filename = filename;
-	}
-
-	public ProjectAssociationImportClient(String filename) {
-		super();
-		setFilename(filename);
-	}
-	public void run() throws IOException{
-		importAssociationsFromCSV(filename);
-	}
-	public static void importAssociationsFromCSV(String filename) throws IOException{
+	public static void importAssociationsFromCSV(String filename) throws IOException, FileNotFoundException{
 		CSVReader reader;
-		try {
-			 reader = new CSVReader(new FileReader(filename));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
+		
+		reader = new CSVReader(new FileReader(filename));
 		//read headers
 		reader.readNext();
 		//parse file
@@ -89,12 +68,13 @@ public class ProjectAssociationImportClient {
 	 public static void main (String[] args){
 		 if (args.length > 1){
 			 System.out.println("Importing " + args[1]);
-//			 ProjectAssociationImportClient importClient;
-//			 importClient = new ProjectAssociationImportClient(args[1]);
 			 try {
 				importAssociationsFromCSV(args[1]);
+			} catch (FileNotFoundException e) {
+				System.err.println("File not found");
+				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				System.err.println("Error whilst reading file");
 				e.printStackTrace();
 			}
 		 }
